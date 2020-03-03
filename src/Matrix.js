b9fb642508070febbec8ce52ba63cb39baaf9155
Emery Tate <tateejt@gmail.com>
2020-03-03 03:05:29 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -24,5 +24,6 @@ export default class Matrix extends Component {
 Matrix.defaultProps = {
   values: (() => {
     const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+    return (new Array(10).fill(defRow))
   })()
 }
